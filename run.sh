f93d8aa9c84bd1ef974df785ce56963aa9e94b91
Anshu <anshu.avinash35@gmail.com>
2013-10-08 16:56:52 +0530

    Make run.sh global

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $1 ]; then
+  if [ -f $1.s ]; then
+    as $1.s --32 -o $1.o
+    ld $1.o -m elf_i386 -o $1
+    ./$1
+    echo "Result: " $?
+    rm -f $1.o $1
+  else
+    echo "$1.s doesn't exist."
+  fi
+else
+  echo "Usage:" $0 "file_name(without_extension)"
+fi
